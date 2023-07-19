1d9ad542d9c3f4263ad4f994fcc8e5a9dcde2189
ecxstacy <dc.dhruvchouhan@gmail.com>
2023-07-19 14:44:33 +0530

    code cleaned + comments added
    commented, cleaned

--- src/filler/engines/doc-extractor-engine.js
+++ src/filler/engines/doc-extractor-engine.js
@@ -12,16 +12,20 @@ export class DocExtractorEngine {
   // will return true if the node is a typical google form section name and heading node
   checkSectionHeading(divNode) {
     // returns if the div selected by div[role=listitem] is a heading or not
+
     // logic : if the first childnode of the selected div has role=heading then it is a section heading
+    // list the attributes
     const attributes = divNode.firstChild.attributes;
 
+    // might Throw TypeError in case the node is a simple div with no attributes.getNamedItem() function, thus calling null() function will throw typeerror
     try {
-      if (attributes.getNamedItem === null) return false;
-      else if (attributes.getNamedItem("role").nodeValue === "heading") {
+      // if it turns out to be heading, then return true
+      if (attributes.getNamedItem("role").nodeValue === "heading") {
         return true;
       }
       return false;
     } catch (typeError) {
+      // in case of typeError, we conclude it cant be heading thus return false 👍
       // console.log(typeError);
       return false;
     }
