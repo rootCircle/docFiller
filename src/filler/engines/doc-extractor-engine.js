147fd80d894a5ce2e62218a0ab833de108e2676b
ecxstacy <dc.dhruvchouhan@gmail.com>
2023-07-19 14:16:12 +0530

    getValidQuestions() excludes the section heading div

--- src/filler/engines/doc-extractor-engine.js
+++ src/filler/engines/doc-extractor-engine.js
@@ -9,6 +9,24 @@ export class DocExtractorEngine {
     };
   }
 
+  // will return true if the node is a typical google form section name and heading node
+  checkSectionHeading(divNode) {
+    // returns if the div selected by div[role=listitem] is a heading or not
+    // logic : if the first childnode of the selected div has role=heading then it is a section heading
+    const attributes = divNode.firstChild.attributes;
+
+    try {
+      if (attributes.getNamedItem === null) return false;
+      else if (attributes.getNamedItem("role").nodeValue === "heading") {
+        return true;
+      }
+      return false;
+    } catch (typeError) {
+      // console.log(typeError);
+      return false;
+    }
+  }
+
   getValidQuestions() {
     // Returns an Array of DOM objects of validated Boxes present in Google Form
     // Return Type : Array
@@ -22,6 +40,7 @@ export class DocExtractorEngine {
     // But, as a garbage returns checkbox too
     // Returns a node list array, where each element is a DOM containing complete information for questions
     // Return Type : NodeList
+
     return document.querySelectorAll("div[role=listitem]");
   }
 
@@ -30,6 +49,14 @@ export class DocExtractorEngine {
     // Weeds out Checkbox collected as a false positive in getQuestions method
     // and returns only the correct ones
     // Return Type : Array
+
+    // ---------- validate if section heading or not ---------
+    // clears the node from questions array if role=listitem includes the section heading (the purple colored one)
+    questions = Array.from(
+      document.querySelectorAll("div[role=listitem]")
+    ).filter((question) => !this.checkSectionHeading(question));
+    // -------------------------------------------------------
+
     return Array.prototype.slice.call(questions).filter((question) => {
       return !this.isCheckBoxListItem(question);
     });
