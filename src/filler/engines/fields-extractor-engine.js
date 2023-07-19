0c8f7860710148778d6ca2e974c2ec90174fd937
Lab Rat <35325046+rootCircle@users.noreply.github.com>
2023-07-19 08:56:48 +0000

    getDescription() will return null to reduce ambiguity if no appropriate type is found! plus minor tweaks documentation

--- src/filler/engines/fields-extractor-engine.js
+++ src/filler/engines/fields-extractor-engine.js
@@ -7,9 +7,10 @@ export class FieldsExtractorEngine {
 
   // Testing on Different Forms required
 	getTitle(element) {
+		// Input Type: DOM Object
 		// Extracts the title of the question
-		// The extraction is based on the DOM tree
-		// The required node is obtained by selecting the first child element of div with role=heading
+		// Tweak : - The extraction is based on the DOM tree
+		//         - The required node is obtained by selecting the first child element of div with role=heading
 		// Return Type : String
 		let required = element.querySelector('div[role="heading"]')
 		required = required.children[0]
@@ -18,14 +19,17 @@ export class FieldsExtractorEngine {
 
 	// Testing on Different Forms required
 	getDescription(element) {
+		// Input Type: DOM Object
 		// Extracts the description of the question
-		// The extraction is based on the DOM tree
-		// The required node is obtained by selecting the second child element of the parent element of div with role=heading
-		// Return Type : String
+		// Tweak : - The extraction is based on the DOM tree
+		//         - The required node is obtained by selecting the second child element of the parent element of div with role=heading
+		// Return Type : String (Returns null if no description is found!)
 		let required = element.querySelector('div[role="heading"]')
 		required = required.parentElement.children[1]
-		if(required === undefined || required.textContent === "")
-		return "There is no description."
+		if(required === undefined || required.textContent === "") {
+			console.log("There is no description for this box!");
+			return null;
+		}
 		return required.textContent
 	}
   
