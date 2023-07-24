1fb81b6b901f786b3497cf1cd200225a3e53bb44
Lab Rat <dev.frolics@gmail.com>
2023-07-24 06:38:15 +0530

    Fixed bug causing multi line title and description to be trimmed to a single line

--- src/filler/engines/fields-extractor-engine.js
+++ src/filler/engines/fields-extractor-engine.js
@@ -12,9 +12,23 @@ export class FieldsExtractorEngine {
 		// Tweak : - The extraction is based on the DOM tree
 		//         - The required node is obtained by selecting the first child element of div with role=heading
 		// Return Type : String
-		let required = element.querySelector('div[role="heading"]')
-		required = required.children[0]
-		return required.textContent
+		let required = element.querySelector('div[role="heading"]');
+		required = required.children[0];
+
+		let content = "";
+
+		// Every new line is either inside a div or independent, hence has nodeName #text
+		Array.from(required.childNodes).forEach(element => {
+			if (element.nodeName === '#text') {
+				content += element.textContent + "\n";
+			}
+			else {
+				content += element.textContent + "\n";
+			}
+		});
+
+		// Remove trailing whitespace at the end
+		return content.trimEnd();
 	}
 
 	// Testing on Different Forms required
@@ -24,13 +38,27 @@ export class FieldsExtractorEngine {
 		// Tweak : - The extraction is based on the DOM tree
 		//         - The required node is obtained by selecting the second child element of the parent element of div with role=heading
 		// Return Type : String (Returns null if no description is found!)
-		let required = element.querySelector('div[role="heading"]')
-		required = required.parentElement.children[1]
+		let required = element.querySelector('div[role="heading"]');
+		required = required.parentElement.children[1];
 		if(required === undefined || required.textContent === "") {
 			console.log("There is no description for this box!");
 			return null;
 		}
-		return required.textContent
+		
+		let content = "";
+
+		// Every new line is either inside a div or independent, hence has nodeName #text
+		Array.from(required.childNodes).forEach(element => {
+			if (element.nodeName === '#text') {
+				content += element.textContent + "\n";
+			}
+			else {
+				content += element.textContent + "\n";
+			}
+		});
+
+		// Remove trailing whitespace at the end
+		return content.trimEnd();
 	}
   
 }
