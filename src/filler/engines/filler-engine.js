54e1284ac4cfd96412d65ff8c227bda91dbf312e
Harshit-Chordiya <harshitchordiya03@gmail.com>
2023-07-30 10:46:03 +0530

    Filler engine in JS

--- src/filler/engines/filler-engine.js
+++ src/filler/engines/filler-engine.js
@@ -1,4 +1,7 @@
 import QType from "../../utils/question-types";
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 export class FillerEngine {
   // Passes in the required field as form of input and fill in those values appropriately
@@ -8,6 +11,7 @@ export class FillerEngine {
 
   fill(element, fieldType, value) {
     if (fieldType != null) {
+      console.log(fieldType)
       if (fieldType === QType.TEXT) {
         return this.fillText(element, value);
       }
@@ -34,7 +38,7 @@ export class FillerEngine {
 
   fillText(element, value) {
     // return true if value is successfully written, else false
-    var inputField = element.querySelectorAll('input[type=text]');
+      var inputField = element.querySelectorAll('input[type=text]');
       inputField=inputField[0];
       inputField.value = value;
       var inputEvent = new Event('input', { bubbles: true });
@@ -106,5 +110,90 @@ fillDate(element, value) {
 }
 
 
+
+async fillDate_And_Time(element, datevariable) {
+
+  await sleep(500);
+  
+  // Validate the date format
+  const datePattern = /^(\d{2})-(\d{2})-(\d{4})$/;
+
+  if (!datePattern.test(datevariable)) {
+    // Invalid format
+    return false; 
+  }
+
+  const [day, month, year] = datevariable.split("-");
+  const date = new Date(`${year}-${month}-${day}`);
+
+  if (!(date.getDate() === Number(day) && date.getMonth() + 1 === Number(month) && date.getFullYear() === Number(year))) {
+    console.log("Invalid date format or an invalid date.");
+    return false;
+  }
+
+  
+  // 1. Fill the day input field
+  var inputField = element.querySelectorAll('input[type=text], input[type=date]');
+  inputField = inputField[0];
+  inputField.value = day;
+  var inputEvent = new Event('input', { bubbles: true });
+  inputField.dispatchEvent(inputEvent);
+
+  // 2. Fill the month input field
+  inputField = element.querySelectorAll('input[type=text], input[type=date]');
+  inputField = inputField[1];
+  inputField.value = month;
+  inputEvent = new Event('input', { bubbles: true });
+  inputField.dispatchEvent(inputEvent);
+
+  // 3. Fill the year input field
+  inputField = element.querySelectorAll('input[type=text], input[type=date]');
+  inputField = inputField[2];
+  inputField.value = year;
+  inputEvent = new Event('input', { bubbles: true });
+  inputField.dispatchEvent(inputEvent);
+
+
+
+  // TIME
+  var inputField = element.querySelectorAll('input[type=text]');
+  inputField=inputField[3];
+  inputField.value = hours;
+  var inputEvent = new Event('input', { bubbles: true });
+  inputField.dispatchEvent(inputEvent)
+  // true
+  var inputField = element.querySelectorAll('input[type=text]');
+  inputField=inputField[4];
+  inputField.value = minutes;
+  var inputEvent = new Event('input', { bubbles: true });
+  inputField.dispatchEvent(inputEvent)
+
+  return true;
+}
+
+// fillDuration(element, value) {
+
+//   // TIME
+//   var inputField = element.querySelectorAll('input[type=text]');
+//   inputField=inputField[3];
+//   inputField.value = hours;
+//   var inputEvent = new Event('input', { bubbles: true });
+//   inputField.dispatchEvent(inputEvent)
+//   // true
+//   var inputField = element.querySelectorAll('input[type=text]');
+//   inputField=inputField[4];
+//   inputField.value = minutes;
+//   var inputEvent = new Event('input', { bubbles: true });
+//   inputField.dispatchEvent(inputEvent)
+
+
+//   var inputField = element.querySelectorAll('input[type=text]');
+//   inputField=inputField[3];
+//   inputField.value = seconds;
+//   var inputEvent = new Event('input', { bubbles: true });
+//   inputField.dispatchEvent(inputEvent)
+
+//   return true;
+// }
 
 }
