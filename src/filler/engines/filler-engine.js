f0da04f943e97b37c260bb95862e4fa726677d94
Lab Rat <dev.frolics@gmail.com>
2023-08-08 02:26:57 +0530

    Changed function name to accord to lower camelcase, some refractoring and style changes

--- src/filler/engines/filler-engine.js
+++ src/filler/engines/filler-engine.js
@@ -1,6 +1,6 @@
 import QType from '../../utils/question-types'
 
-function sleep (milliseconds) {
+function sleep(milliseconds) {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 
@@ -8,23 +8,23 @@ export class FillerEngine {
   // Passes in the required field as form of input and fill in those values appropriately
   // via DOM
 
-  constructor () {}
+  constructor() { }
 
   fill(element, fieldType, fieldValue, value) {
     if (fieldType != null) {
       console.log(fieldType)
       if (fieldType === QType.TEXT) {
-        return this.fillText(element, fieldValue,"this is text")
+        return this.fillText(fieldValue, "this is text")
       } else if (fieldType === QType.TEXT_EMAIL) {
-        return this.fillEmail(element, fieldValue,'harshit123@gmail.com')
+        return this.fillEmail(fieldValue, 'harshit123@gmail.com')
       } else if (fieldType === QType.PARAGRAPH) {
-        return this.fillParagraph(element, fieldValue,"this is a paragraph ")
+        return this.fillParagraph(fieldValue, "this is a paragraph ")
       } else if (fieldType === QType.DATE) {
-        return this.fillDate(element, fieldValue,"11-11-2111")
+        return this.fillDate(fieldValue, "11-11-2111")
       } else if (fieldType === QType.DATE_AND_TIME) {
-        return this.fillDate_And_Time(element, fieldValue, '01-01-2003-01-01')
+        return this.fillDateAndTime(fieldValue, '01-01-2003-01-01')
       } else if (fieldType === QType.TIME) {
-        return this.fillTime(element,fieldValue, '02-02')
+        return this.fillTime(fieldValue, '02-02')
       } else if (
         fieldType === QType.MULTI_CORRECT_WITH_OTHER ||
         fieldType === QType.MULTI_CORRECT
@@ -35,11 +35,11 @@ export class FillerEngine {
           { optionTitle: 'Other:', optionText: 'My name is Monark Jain' }
         ])
       } else if (fieldType === QType.DURATION) {
-        return this.fillDuration(element, fieldValue, '11-11-11')
+        return this.fillDuration(fieldValue, '11-11-11')
       } else if (fieldType === QType.DATE_WITHOUT_YEAR) {
-        return this.fillDate_Without_Year(element, fieldValue, '11-11')
+        return this.fillDateWithoutYear(fieldValue, '11-11')
       } else if (fieldType === QType.DATE_TIME_WITHOUT_YEAR) {
-        return this.fillDate_Time_Without_Year(element, fieldValue, '22-01-01-01')
+        return this.fillDateTimeWithoutYear(fieldValue, '22-01-01-01')
       } else {
         return false
       }
@@ -48,57 +48,57 @@ export class FillerEngine {
     }
   }
 
-  fillText (element, fieldValue, value) {
+  fillText(fieldValue, value) {
+    // Input Type : Extracted Object(fieldValue) & string(value)
+    // Valid value format :- Single line string
     // Function to fill question with type text input with a specified value.
-    // Input Type : DOM object(element) & string(value)
-    // Tweak : 
     // Return Type : Boolean
-    // console.log(fieldValue.dom[0]);
-   let inputField = fieldValue.dom[0]
+
+    let inputField = fieldValue.dom[0]
     inputField.value = value
-    var inputEvent = new Event('input', { bubbles: true })
+    let inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     inputField.dispatchEvent(inputEvent)
     return true
   }
 
-  fillEmail (element, fieldValue, value) {
+  fillEmail(fieldValue, value) {
+    // Input Type : Extracted Object(fieldValue) & string(value)
+    // Valid value format :- An valid Email
     // Function to fill email
-    // Input Type : DOM object(element) & string(value)
-    // Tweak : 
     // Return Type : Boolean
-    let inputField = fieldValue.dom[0]
-    inputField.value =value
-    var inputEvent = new Event('input', { bubbles: true })
-    // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
-    inputField.dispatchEvent(inputEvent)
-    return true
-  }
 
-  fillParagraph (element, fieldValue, value) {
-    // Function to fill paragraph type questions
-    // Input Type : DOM object(element) & string(value)
-    // Tweak : 
-    // Return Type : Boolean
     let inputField = fieldValue.dom[0]
     inputField.value = value
-    var inputEvent = new Event('input', { bubbles: true })
+    let inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     inputField.dispatchEvent(inputEvent)
     return true
   }
 
-  
-  //*********** it may raise a problem if format is different need to look into this  ************/
-  
-  fillDate (element, fieldValue, value) {
-    // Function to fill date
-    // Input Type : DOM object(element) & string(value)
-    // Tweak : 
+  fillParagraph(fieldValue, value) {
+    // Input Type : Extracted Object(fieldValue) & string(value)
+    // Valid value format :- A paragraph (can be multiline too)
+    // Function to fill paragraph type questions
     // Return Type : Boolean
-    // value format :- "dd-mm-yyyy"
+
+    let inputField = fieldValue.dom[0]
+    inputField.value = value
+    let inputEvent = new Event('input', { bubbles: true })
+    // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
+    inputField.dispatchEvent(inputEvent)
+    return true
+  }
+
+  fillDate(fieldValue, value) {
+    // Input Type : Extracted Object(fieldValue) & string(value)
+    // Valid Value format :- "dd-mm-yyyy"
+    // Function to fill date
+    // Return Type : Boolean
+
     // Validate the date format
     const datePattern = /^(\d{2})-(\d{2})-(\d{4})$/
+    let inputEvent = new Event('input', { bubbles: true })
 
     if (!datePattern.test(value)) {
       // Invalid format
@@ -108,249 +108,243 @@ export class FillerEngine {
     const [day, month, year] = value.split('-')
     const date = new Date(`${year}-${month}-${day}`)
 
+    // Invalid date format as raised by date constructor
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date#return_value
+    if (date.valueOf() === NaN) {
+      return false;
+    }
+
+    // To ensure any params to be filled isn't missed in Date constructor
     if (
       !(
         date.getDate() === Number(day) &&
-        date.getMonth() + 1 === Number(month) &&
+        date.getMonth() + 1 === Number(month) && // Month numbering start from 0...11
         date.getFullYear() === Number(year)
       )
     ) {
-      console.log('Invalid date format or an invalid date.')
-      return false
+      return false;
     }
 
     // 1. Fill the day input field
-   let dd = fieldValue.dom[0]
+    let dd = fieldValue.dom[0]
     dd.value = day
-    var inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     dd.dispatchEvent(inputEvent)
 
     // 2. Fill the month input field
-   let mm = fieldValue.dom[1]
+    let mm = fieldValue.dom[1]
     mm.value = month
-    inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     mm.dispatchEvent(inputEvent)
 
     // 3. Fill the year input field
-   let yyyy = fieldValue.dom[2]
+    let yyyy = fieldValue.dom[2]
     yyyy.value = year
-    inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     yyyy.dispatchEvent(inputEvent)
 
     return true
   }
 
-  async fillDate_And_Time (element, fieldValue, datevariable) {
+  async fillDateAndTime(fieldValue, value) {
     // Function to fill date and time
-    // Input Type : DOM object(element) & datevariable(value)
+    // Input Type : Extracted Object(fieldValue) & string(value)
     // Tweak : 
     // Return Type : Boolean
-    // value format :- "dd-mm-yyyy-hh-mm"
+    // Valid Value format :- "dd-mm-yyyy-hh-mm"
     // hh in 24 hrs format
 
     //sleep done because form was overriding values set by this function before
-    await sleep(1500)
+    await sleep(2000)
 
+    let inputEvent = new Event('input', { bubbles: true })
     // Validate the date format
-    const datePattern = /^(\d{2})-(\d{2})-(\d{4})$/
-
-    const [day, month, year, hours, minutes] = datevariable.split('-')
+    const datePattern = /^(\d{2})-(\d{2})-(\d{4})-(\d{2})-(\d{2})$/
+    if (!datePattern.test(value)) {
+      // Invalid format
+      return false
+    }
+    const [day, month, year, hours, minutes] = value.split('-')
     const date = new Date(`${year}-${month}-${day}`)
 
+    // Invalid date format as raised by date constructor
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/Date#return_value
+    if (date.valueOf() === NaN) {
+      return false;
+    }
+
+    // To ensure any params to be filled isn't missed in Date constructor
     if (
       !(
         date.getDate() === Number(day) &&
-        date.getMonth() + 1 === Number(month) &&
+        date.getMonth() + 1 === Number(month) && // Month numbering start from 0...11
         date.getFullYear() === Number(year)
       )
     ) {
-      console.log('Invalid date format or an invalid date.')
       return false
     }
 
     // 1. Fill the day input field
-   let dd = fieldValue.dom[0]
-     dd.value = day
-    var inputEvent = new Event('input', { bubbles: true })
+    let dd = fieldValue.dom[0]
+    dd.value = day
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     dd.dispatchEvent(inputEvent)
 
     // 2. Fill the month input field
-    // inputField = element.querySelectorAll('input[type=text], input[type=date]')
-   let mm = fieldValue.dom[1]
+    let mm = fieldValue.dom[1]
     mm.value = month
-    inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     mm.dispatchEvent(inputEvent)
 
     // 3. Fill the year input field
-    // inputField = element.querySelectorAll('input[type=text], input[type=date]')
-   let yyyy = fieldValue.dom[2]
+    let yyyy = fieldValue.dom[2]
     yyyy.value = year
-    inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     yyyy.dispatchEvent(inputEvent)
 
     // TIME
 
-    //hours
-    // var inputField = element.querySelectorAll('input[type=text]')
+    // hours
     let hh = fieldValue.dom[3]
     hh.value = hours
-    var inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     hh.dispatchEvent(inputEvent)
 
-    //minutes
-    // var inputField = element.querySelectorAll('input[type=text]')
+    // minutes
     let min = fieldValue.dom[4]
     min.value = minutes
-    var inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     min.dispatchEvent(inputEvent)
 
     return true
   }
 
-  fillTime (element, fieldValue, value) {
+  fillTime(fieldValue, value) {
+    // Input Type : Extracted Object(fieldValue) & string(value)
+    // Valid Value format :- "hh-mm"
     // Function to fill time
-    // Input Type : DOM object(element) & string(value)
-    // Tweak : 
     // Return Type : Boolean
-    // value format :- "hh-mm"
     // hh in 24 hrs format
+
+    let inputEvent = new Event('input', { bubbles: true })
     const [hours, minutes] = value.split('-')
 
     // TIME
 
-    //hours
-   let hh = fieldValue.dom[0]
+    // hours
+    let hh = fieldValue.dom[0]
     hh.value = hours
-    var inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners boundmin hh.dispatchEvent(inputEvent)
     hh.dispatchEvent(inputEvent)
 
-    //minutes
-    // var inputField = element.querySelectorAll('input[type=text]')
-  let  min =fieldValue.dom[1]
+    // minutes
+    let min = fieldValue.dom[1]
     min.value = minutes
-    var inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     min.dispatchEvent(inputEvent)
 
     return true
   }
 
-  fillDuration (element, fieldValue, value) {
+  fillDuration(fieldValue, value) {
+    // Input Type : Extracted Object(fieldValue) & string(value)
+    // Valid Value format :- "hh-mm-ss"
     // Function to fill time
-    // Input Type : DOM object(element) & string(value)
-    // Tweak : 
     // Return Type : Boolean
-    // value format :- "hh-mm-ss"
     // hh in 24 hrs format
+
     const [hours, minutes, seconds] = value.split('-')
+    let inputEvent = new Event('input', { bubbles: true })
 
     // TIME
 
-    //seconds
+    // hours
     let hh = fieldValue.dom[0]
     hh.value = hours
-    var inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     hh.dispatchEvent(inputEvent)
 
-    //minutes
-    let mm =fieldValue.dom[1]
+    // minutes
+    let mm = fieldValue.dom[1]
     mm.value = minutes
-    var inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     mm.dispatchEvent(inputEvent)
 
-    //seconds
-    // var inputField = element.querySelectorAll('input[type=text]')
+    // seconds
     let ss = fieldValue.dom[2]
     ss.value = seconds
-    var inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     ss.dispatchEvent(inputEvent)
 
     return true
   }
 
-  fillDate_Without_Year (element, fieldValue, value) {
+  fillDateWithoutYear(fieldValue, value) {
+    // Input Type : Extracted Object(fieldValue) & string(value)
+    // Valid Value format :- "dd-mm"
     // Function to fill date without year
-    // value format :- "dd-mm"
-    // Input Type : DOM object(element) & string(value)
-    // Tweak : 
     // Return Type : Boolean
+
     const [day, month] = value.split('-')
+    let inputEvent = new Event('input', { bubbles: true })
 
     // 1. Fill the day input field
     let dd = fieldValue.dom[0]
     dd.value = day
-    var inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     dd.dispatchEvent(inputEvent)
 
     // 2. Fill the month input field
-   let mm =fieldValue.dom[1]
+    let mm = fieldValue.dom[1]
     mm.value = month
-    inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     mm.dispatchEvent(inputEvent)
 
     return true
   }
 
-  async fillDate_Time_Without_Year (element, fieldValue, value) {
-    // Function to fill time
-    // Input Type : DOM object(element) & string(value)
-    // Tweak : 
+  async fillDateTimeWithoutYear(fieldValue, value) {
+    // Input Type : Extracted Object(fieldValue) & string(value)
+    // Valid Value format :- "dd-mm-hh-mm"
+    // Function to fill date and time not having year
     // Return Type : Boolean
-    // value format :- "dd-mm-hh-mm"
     // hh in 24 hrs format
 
-     //! to avoid the race around between our answer and default NULL value
-     await sleep(2000)
-     
+    //! to avoid the race around between our answer and default NULL value
+    await sleep(2000)
+
+    let inputEvent = new Event('input', { bubbles: true })
     const [day, month, hours, minutes] = value.split('-')
 
     // 1. Fill the day input field
-   let dd =fieldValue.dom[0]
+    let dd = fieldValue.dom[0]
     dd.value = day
-    var inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     dd.dispatchEvent(inputEvent)
 
     // 2. Fill the month input field
-   let mm = fieldValue.dom[1]
+    let mm = fieldValue.dom[1]
     mm.value = month
-    inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     mm.dispatchEvent(inputEvent)
 
     // TIME
 
-    //hours
-   let hh = fieldValue.dom[2]
+    // hours
+    let hh = fieldValue.dom[2]
     hh.value = hours
-    var inputEvent = new Event('input', { bubbles: true })
     // Dispatch the 'input' event on the input field to trigger any event listeners bound to it.
     hh.dispatchEvent(inputEvent)
 
-    //minutes
-   let min = fieldValue.dom[3]
+    // minutes
+    let min = fieldValue.dom[3]
     min.value = minutes
-    var inputEvent = new Event('input', { bubbles: true })
     min.dispatchEvent(inputEvent)
 
     return true
   }
 
-  async fillCheckBox (element, value) {
+  async fillCheckBox(element, value) {
     //! To avoid any unwanted and incomplete answer this sleep function is necessary
     //! to avoid the race around between our answer and default NULL value
     await sleep(2000)
@@ -373,8 +367,10 @@ export class FillerEngine {
       let otherOption = element.querySelectorAll(
         'div[data-other-checkbox=true]'
       )
+
+      let otherOptionName = null;
       if (otherOption.length != 0) {
-        var otherOptionName = otherOption[0].getAttribute('aria-label')
+        otherOptionName = otherOption[0].getAttribute('aria-label')
       }
 
       value.forEach(val => {
