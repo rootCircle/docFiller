c2c94a83369fcec8aab153063591e973802a065d
Lab Rat <dev.frolics@gmail.com>
2023-08-08 12:57:57 +0530

    Null checking and changed function names to removed ambiguity

--- src/filler/engines/parser-engine.js
+++ src/filler/engines/parser-engine.js
@@ -26,13 +26,13 @@ export class ParserEngine {
                 return this.parseMultipleChoice(value, response);
             }
             else if (fieldType === QType.DATE) {
-                return this.parseDate(response);
+                return this.validateDate(response);
             }
             else if (fieldType === QType.DATE_AND_TIME) {
-                return this.parseDateAndTime(response);
+                return this.validateDateAndTime(response);
             }
             else if (fieldType === QType.TIME) {
-                return this.parseTime(response);
+                return this.validateTime(response);
             }
             else {
                 return null;
@@ -57,59 +57,79 @@ export class ParserEngine {
         // Return Type : Boolean
 
         let mailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-        return Boolean(response.match(mailRegex));
+        return response && Boolean(response.match(mailRegex));
     }
 
     validateDate(response) {
-        //Checks If date is in correct format
+        // Input :- String
+        // Valid Input Format :- DD-MM--YYYY
+        // Checks if date is in correct format or not
+        // Output :- Boolean
+
         var dateValid = /^([0][1-9]|[1-2][0-9]|[3][0-1])-([0][1-9]|[1][0-2])-(\d{4})$/
-        const dateArr = response.match(dateValid)
+        const dateArr = response === null ? false : response.match(dateValid)
         if (dateArr) {
+
+            // Months containing 31 days
             const valid31 = ['01', '03', '05', '07', '08', '10', '12']
             const date = dateArr[1]
             const month = dateArr[2]
             const year = Number(dateArr[3])
+
+
             if (date == '31') {
                 if (!valid31.includes(month))
                     return false;
             }
             else if (date == '30') {
-                if (month == '02')
-                    return false;
+                if (month == '02') {
+                    return false; // February doesn't have 30 days
+                }
             }
             else if (date == '29') {
                 if (month == '02') {
                     if (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0))
-                        return false;
+                        return false; // Non-leap year doesn't have 29 days
                 }
             }
             return true
         }
-        else
+        else {
             return false
+        }
     }
 
-    parseDateAndTime(response) {
-        //Checks If date time is in correct format
+    validateDateAndTime(response) {
+        // Input Type :- String
+        // Valid Input Format :- DD-MM-YYYY-HH-MM
+        // HH is in 24 hrs format
+        // Checks if date & time is in correct format
+        // Output Type :- Boolean
+
         var dateTimeValid = /^([0][1-9]|[1-2][0-9]|[3][0-1])-([0][1-9]|[1][0-2])-(\d{4})-([01][0-9]|[2][0-3])-([0-5][0-9])$/
-        const dateArr = response.match(dateTimeValid)
+        const dateArr = response === null ? false : response.match(dateTimeValid)
         if (dateArr) {
+            
+            // Months containing 31 days
             const valid31 = ['01', '03', '05', '07', '08', '10', '12']
             const date = dateArr[1]
             const month = dateArr[2]
             const year = Number(dateArr[3])
+
+
             if (date == '31') {
                 if (!valid31.includes(month))
                     return false;
             }
             else if (date == '30') {
-                if (month == '02')
-                    return false;
+                if (month == '02') {
+                    return false; // February doesn't have 30 days
+                }
             }
             else if (date == '29') {
                 if (month == '02') {
                     if (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0))
-                        return false;
+                        return false; // Non-leap year doesn't have 29 days
                 }
             }
             return true
@@ -118,12 +138,15 @@ export class ParserEngine {
             return false
     }
 
-    parseTime(response) {
+    validateTime(response) {
+        // Input Type :- String
+        // Valid Input Format :- HH-MM
+        // HH is in 24 hrs format
+        // Checks if time is in correct format
+        // Output Type :- Boolean
+
         var timeValid = /^([01][0-9]|[2][0-3])-([0-5][0-9])$/
-        if (response.match(timeValid))
-            return true;
-        else
-            return false;
+        return response && Boolean(response.match(timeValid));
     }
 
     parseMultipleChoice(value, response) {
