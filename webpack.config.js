0f2ce6163d5de1ace3010edd9114059e90ed9490
Lab Rat <dev.frolics@gmail.com>
2023-08-13 15:51:25 +0530

    Setup up env vars

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+require('dotenv').config({ path: 'develop.env' })
+const webpack = require('webpack')
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -19,6 +22,12 @@ module.exports = {
         { from: "src/filler", to: "filler" },
         { from: "src/icons", to: "icons" },
       ],
+    }),
+    new NodePolyfillPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        OPENAI_API_KEY: JSON.stringify(process.env.OPENAI_API_KEY)
+      }
     }),
   ],
 };
