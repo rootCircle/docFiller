7d2a82623eefc1b193fe95348e52ac407c20f78e
Lab Rat <dev.frolics@gmail.com>
2023-08-26 18:43:22 +0530

    chore: initial structure setup for gpt generator engine

--- src/filler.js
+++ src/filler.js
@@ -5,6 +5,7 @@ import { FieldsExtractorEngine } from "./filler/engines/fields-extractor-engine"
 import { DetectBoxType } from "./filler/detectors/detect-box-type";
 import { PromptEngine } from "./filler/engines/prompt-engine";
 import { ParserEngine } from "./filler/engines/parser-engine";
+import { GeneratorEngine } from "./filler/engines/gpt-engine";
 import GPTEngine from "./utils/gpt-engines";
 
 let debugging = true;
@@ -38,7 +39,8 @@ async function main() {
 	let checker = new DetectBoxType();
 	let fields = new FieldsExtractorEngine();
 	let filler = new FillerEngine();
-	let prompt = new PromptEngine(GPTEngine.CHATGPT);
+	let prompt = new PromptEngine();
+	let gpt = new GeneratorEngine(GPTEngine.CHATGPT);
 	let parser = new ParserEngine();
 
 	questions.forEach(question => {
@@ -46,62 +48,30 @@ async function main() {
 		if (fieldType !== null) {
 
 			let fieldValue = fields.getFields(question, fieldType);
-			let promptText = prompt.prompt(fieldType, fieldValue);
-			let response = prompt.getResponse(fieldType, fieldValue);
-			let parsed_response = parser.parse(fieldType, fieldValue, response);
+			let promptText = prompt.getPrompt(fieldType, fieldValue);
+			Promise.resolve(gpt.getResponse(prompt)).then((response) => {
+				let parsed_response = parser.parse(fieldType, fieldValue, response);
 
-			// Using Dummy Value for brevity
-			Promise.resolve(filler.fill(question, fieldType, fieldValue, "Dummy Value")).then((filled) => {
-				console.log(question)
+				// Using Dummy Value for brevity
+				Promise.resolve(filler.fill(question, fieldType, fieldValue, "Dummy Value")).then((filled) => {
+					console.log(question)
 
-				console.log("Field Type : " + fieldType);
-				console.log("Fields ↴")
+					console.log("Field Type : " + fieldType);
+					console.log("Fields ↴")
 
-				console.log("Field Value ↴")
-				console.log(fieldValue);
+					console.log("Field Value ↴")
+					console.log(fieldValue);
 
 
-				console.log("Prompt for Model : " + promptText);
-				console.log("Response : " + response);
-				console.log("Parsed Response : " + parsed_response)
-				console.log("Filled : " + filled);
-				console.log();	
+					console.log("Prompt for Model : " + promptText);
+					console.log("Response : " + response);
+					console.log("Parsed Response : " + parsed_response)
+					console.log("Filled : " + filled);
+					console.log();
+				});
 			});
+
 		}
 	});
 
 }
-
-
-
-
-
-///////////////////////////////////////////////////////////////////
-// Dead Code down here. (Might be used later)
-///////////////////////////////////////////////////////////////////
-
-// // alert("Bruhhhhh multipler x10");
-// var inputs = document.querySelectorAll('input[type="text"], input[type="number"], input[type="email"]');
-
-// console.log(document.querySelectorAll("div[role=radio]"))
-
-// // Fill each input field with the value "bruh@gmail.com"
-// // Missed one include textarea etc, radios, checkbox
-// inputs.forEach(input => {
-//   console.log(input)
-//   input.value = "bruh@gmail.com";
-//   var inputEvent = new Event('input', { bubbles: true });
-//   input.dispatchEvent(inputEvent)
-// });
-
-// class Data {
-//   constructor(pageNo, sectionID, data, questionStatement, assumedImageContext, outputType, value) {
-//     this.pageNo = pageNo;
-//     this.sectionID = sectionID;
-//     this.data = data;
-//     this.questionStatement = questionStatement;
-//     this.assumedImageContext = assumedImageContext;
-//     this.outputType = outputType;
-//     this.value = value;
-//   }
-// }
